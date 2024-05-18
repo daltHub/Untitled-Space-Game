7aaf06cca04ac498877daeabfc0c8f2c63e8252f
daltHub <57568091+daltHub@users.noreply.github.com>
2024-05-18 23:36:05 +0100

    changed timer to code, added janky label

--- main.gd
+++ main.gd
@@ -2,18 +2,28 @@ extends Node
 @onready var health_bar = $HUD/HealthBar
 @onready var player = $Player
 var enemy = preload("res://enemy.tscn")
+var level_time_limit = 60.0
+var level_timer : Timer = Timer.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$EnemySpawnTimer.start(	)
-	$LevelTimer.start()
-	pass # Replace with function body.
+	# set up level timer
+	level_timer.one_shot = true
+	level_timer.wait_time = level_time_limit
+	level_timer.autostart = false
+	level_timer.timeout.connect(_on_level_timer_timeout)
+	add_child(level_timer)
+	level_timer.start()
+	
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var player_health = player.player_health
 	health_bar.set_value_no_signal(player_health)
-	pass
+	$HUD/LevelTimerLabel.text = str(int(level_timer.time_left))
+
 
 func new_game():
 	get_tree().reload_current_scene()
@@ -24,6 +34,8 @@ func game_over():
 	new_game()
 	pass
 
+func _on_level_timer_timeout():
+	new_game()
 
 func _on_enemy_spawn_timer_timeout():
 	var mob = enemy.instantiate()
@@ -42,6 +54,3 @@ func _on_enemy_spawn_timer_timeout():
 	add_child(mob)
 	pass # Replace with function body.
 
-
-func _on_level_timer_timeout():
-	new_game()
